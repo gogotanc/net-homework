5020850e615daa46dfa78cbb128a299ce30cf0f6
tanc <gogotanc@163.com>
2018-01-31 14:38:19 +0800

    user service done

--- src/main/java/cn/gogotanc/work/service/impl/UserServiceImpl.java
+++ src/main/java/cn/gogotanc/work/service/impl/UserServiceImpl.java
@@ -1,6 +1,7 @@
 package cn.gogotanc.work.service.impl;
 
 import cn.gogotanc.work.service.UserService;
+import cn.gogotanc.work.utils.Constant;
 import org.springframework.stereotype.Service;
 
 /**
@@ -9,8 +10,27 @@ import org.springframework.stereotype.Service;
 @Service
 public class UserServiceImpl implements UserService {
 
+    private static final String BUYER = "buyer";
+    private static final String SELLER = "seller";
+    private static final String BUYER_PASS_MD5 = "37254660E226EA65CE6F1EFD54233424";
+    private static final String SELLER_PASS_MD5 = "981C57A5CFB0F868E064904B8745766F";
+
     @Override
     public int login(String username, String password) {
-        return 0;
+        int resultCode = Constant.RESULT_CODE_ERROR;
+        switch (username) {
+            case BUYER:
+                if (BUYER_PASS_MD5.equalsIgnoreCase(password)) {
+                    resultCode = Constant.IDENTITY_BUYER;
+                }
+                break;
+            case SELLER:
+                if (SELLER_PASS_MD5.equalsIgnoreCase(password)) {
+                    resultCode = Constant.IDENTITY_SELLER;
+                }
+                break;
+            default:
+        }
+        return resultCode;
     }
 }
--- src/main/java/cn/gogotanc/work/utils/Constant.java
+++ src/main/java/cn/gogotanc/work/utils/Constant.java
@@ -2,6 +2,17 @@ package cn.gogotanc.work.utils;
 
 public class Constant {
 
+    /**
+     * 商品是否被购买的标识
+     */
     public static final int GOODS_FLAG_SELLED = 1;
     public static final int GOODS_FLAG_UNSELLED = 0;
+
+    /**
+     * 用户身份标识
+     */
+    public static final int IDENTITY_SELLER = 0;
+    public static final int IDENTITY_BUYER = 1;
+
+    public static final int RESULT_CODE_ERROR = -1;
 }
